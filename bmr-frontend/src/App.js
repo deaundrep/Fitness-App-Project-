7fe44bda023ab73514e42c6e488f163a628a3ecd
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2021-04-26 15:59:43 -0400

    main

--- bmr-frontend/src/App.js
+++ bmr-frontend/src/App.js
@@ -1,14 +1,34 @@
-import { useState } from "react";
-import Search from "./Search/Search";
-import './App.css';
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import red  from "@material-ui/core/colors/red";
+import { BrowserRouter as Router } from "react-router-dom";
+import MainRouter from "./MainRouter";
+import React from "react";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: red[500],
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#FFF5EE",
+      contrastText: "black",
+    },
+  },
+  typography: {
+    fontFamily: ["Roboto", "Helvetica Neue", "Arial", "sans-serif"].join(","),
+  },
+  spacing: 10,
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+    <Router>
+          <MainRouter />
+        </Router>
+    </ThemeProvider>
   );
 }
 

ae654d68890b4346b34c181e1696ad1b00db39ee
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2021-04-26 15:06:27 -0400

    json

---
+++ bmr-frontend/src/App.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Search from "./Search/Search";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;

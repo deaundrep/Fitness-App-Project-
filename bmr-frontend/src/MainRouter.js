2114ad9add13fedb34b7673876934d24241abc44
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2021-04-27 15:38:29 -0400

    hooks

--- bmr-frontend/src/MainRouter.js
+++ bmr-frontend/src/MainRouter.js
@@ -3,13 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 const Home = React.lazy(() => import("./components/Home/Home"));
 const Navbar = React.lazy(() => import("./components/Navbar/Navbar"));
+const Signup = React.lazy(() => import("./components/Signup/Signup"));
 
 function MainRouter() {
     return (
         <>
             <Navbar />
             <Switch>
-
+            <Route path="/sign-up" component={Signup} />
             <Route path="/" component={Home}/>
             </Switch>
             

ae6cf7ce02950d02659293c5cffa622ce880d5b6
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2021-04-27 11:35:19 -0400

    add vid

--- bmr-frontend/src/components/Home/Home.js
+++ bmr-frontend/src/components/Home/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
-import Image from 'material-ui-image'
+//import backgroundImage from "./quote.jpeg"
+import kick from './kick.mp4';
 
 function Home() {
-    return <div> Home </div>
+    return <div> Home
+        <video className='videoTag' autoPlay loop muted>
+    <source src={kick} type='video/mp4' />
+</video>
+        
+        </div>
 }
 
 export default Home;

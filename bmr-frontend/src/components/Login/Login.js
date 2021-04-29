df725fc5c33a7a626f113b30579ad24919c54da4
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2021-04-28 21:57:13 -0400

    working login

--- bmr-frontend/src/components/Login/Login.js
+++ bmr-frontend/src/components/Login/Login.js
@@ -1,39 +1,14 @@
-import React, { useState, useContext, useEffect } from "react";
-import { AuthContext } from "../context/AuthContext";
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+
 
 function Login() {
-    const [user, setUser] = useState(null);
-    const [value, setValue] = useState("");
-
-    const { dispatch } = useContext(AuthContext);
-
-    function handleLogin() {
-        setUser({
-            email: value,
-            password: ""
-        });
-    }
-
-    useEffect(() => {
-        if (user) {
-            dispatch({ type: "LOGIN", user: user });
-        }
-    }, [user]);
-
     return (
         <div>
-            Login
-            <div>
-                <input
-                    name="email"
-                    type="text"
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                />
-                <button onClick={handleLogin}>Login</button>
-            </div>
+            
         </div>
-    );
+    )
 }
 
-export default Login;
+export default Login
